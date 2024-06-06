33a5fb791a31bff852f809f510760c1dacaab262
Fernando Altamirano <alt.nando@gmail.com>
2024-06-05 19:08:46 -0600

    animation to the infoDrawer

--- src/components/InfoDrawer/InfoDrawer.js
+++ src/components/InfoDrawer/InfoDrawer.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
@@ -13,11 +13,12 @@ import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
 import Button from '@mui/material/Button'; // Import Button for reopening drawer
 import customStyles from './InfoDrawer.module.css';
+import 'animate.css';
 
 const drawerWidth = 240;
 
 const InfoDrawer = () => {
-  const [isOpen, setIsOpen] = React.useState(true);
+  const [isOpen, setIsOpen] = useState(true);
 
   // Get today's date
   const today = new Date();
@@ -31,62 +32,65 @@ const InfoDrawer = () => {
     setIsOpen(false);
   };
 
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
+  useEffect(() => {
+    if (!isOpen) {
+      const timeoutId = setTimeout(() => {
+        setIsOpen(false);
+      }, 2000); // Adjust the timeout value according to your animation duration
+      return () => clearTimeout(timeoutId);
+    }
+  }, [isOpen]);
 
   return (
     <div>
-      <Button onClick={handleOpen} style={{ display: isOpen ? 'none' : 'block' }}>Open Drawer</Button>
-      {isOpen && (
-        <Drawer
-          variant="persistent"
-          anchor="right"
-          open={isOpen}
-          sx={{
-            width: drawerWidth,
-            flexShrink: 0,
-            [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-          }}
-          className={customStyles.InfoDrawer}
-        >
-          <Box className={customStyles.header}>
-            <Box component="section">
-              {formattedDate}
-            </Box>
-            <IconButton onClick={handleClose}>
-              <CloseIcon />
-            </IconButton>
+      <Button onClick={() => setIsOpen(true)} style={{ display: isOpen ? 'none' : 'block' }}>Open Drawer</Button>
+      <Drawer
+        variant="persistent"
+        anchor="right"
+        open={isOpen}
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+        className={`${customStyles.InfoDrawer} ${isOpen ? 'animate__animated animate__fadeInRight' : 'animate__animated animate__fadeOutRight'}`}
+      >
+        <Box className={customStyles.header} >
+          <Box component="section">
+            {formattedDate}
           </Box>
-          <Box sx={{ overflow: 'auto' }}>
-            <List>
-              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-            <Divider />
-            <List>
-              {['All mail', 'Trash', 'Spam'].map((text, index) => (
-                <ListItem key={text} disablePadding>
-                  <ListItemButton>
-                    <ListItemIcon>
-                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                    </ListItemIcon>
-                    <ListItemText primary={text} />
-                  </ListItemButton>
-                </ListItem>
-              ))}
-            </List>
-          </Box>
-        </Drawer>
-      )}
+          <IconButton onClick={handleClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <List>
+            {['All mail', 'Trash', 'Spam'].map((text, index) => (
+              <ListItem key={text} disablePadding>
+                <ListItemButton>
+                  <ListItemIcon>
+                    {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      </Drawer>
     </div>
   );
 };
